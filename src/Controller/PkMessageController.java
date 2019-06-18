9cf91f4e6c0f6d4e8e36841aba88083446cd7c4c
jianghong <1260624278@qq.com>
2019-06-18 14:32:40 +0800

    jh singer

--- src/Controller/PkMessageController.java
+++ src/Controller/PkMessageController.java
@@ -1,5 +1,14 @@
 package Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+@Controller
 public class PkMessageController {
-
+	
+	@RequestMapping("/add_pk_message")
+	public ModelAndView add_pk_message() {
+		ModelAndView mView = new ModelAndView("add_pk_message");
+		return mView;
+	}
 }
